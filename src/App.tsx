0c09d945014e008c1c2982d885e2c78ed0df8b25
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2026-09-13 01:44:11 -0500

    refactor(styles): se actualizo el viteconfig para recibir el tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
 	return (
 		<div>
-			<h2>Ejemplo de Componente</h2>
+			<h2 className="text-2xl font-bold">Ejemplo de Componente</h2>
 			<p>
 				Este es un ejemplo de cómo se pueden estructurar los componentes en
 				React con Vite.
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
