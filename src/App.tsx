00874900008f4447382dcbe0b70ae3350f59b6fd
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2026-09-13 16:33:00 -0500

    chore(config): se actualizo para permitir que el prettier toque archivos tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 function App() {
-	return (
-		<div>
-			<h2 className="text-2xl font-bold">Ejemplo de Componente</h2>
-			<p>
-				Este es un ejemplo de cómo se pueden estructurar los componentes en
-				React con Vite.
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Ejemplo de Componente</h2>
+      <p>
+        Este es un ejemplo de cómo se pueden estructurar los componentes en
+        React con Vite.
+      </p>
+    </div>
+  );
 }
 
 export default App;
